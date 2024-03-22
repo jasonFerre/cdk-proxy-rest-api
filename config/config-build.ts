56ed9e99183e582229b680ba83ed1bb8b47bfa96
JasonFerre <geysonb04@gmail.com>
2024-03-22 11:09:14 +0000

    github action

--- config/config-build.ts
+++ config/config-build.ts
@@ -6,43 +6,33 @@ import { Config } from './config'
 type SsmParameter = {
   [key: string]: string
 }
-type subnet = {
-  subnetId: string
-  az: string
-}
-type Network = {
-  region: string
-  vpcId: string
-  subnets: subnet[]
-  vpcCIDR: string
-  securityGroup: string
-}
 
 export type ConfigBuild = {
   projectName: string
   ssmParameters: SsmParameter
-  network: Network[]
   env?: Environment
   envName: string
-  organizationId: string
   envVariables: { [key: string]: string }
 }
 
 const environmentName = process.env.ENVIRONMENTNAME ?? 'dev'
-const regionEnvironment = process.env.AWSREGION ?? 'us-east-1'
 
 console.log(`environmentName: ${environmentName}`)
 export const buildEnvironment = () => {
-  console.log(`aws pipeline region environment ${regionEnvironment}`)
+  console.log(`aws pipeline region environment ${process.env.AWS_DEFAULT_REGION}`)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const jsonSettings = Config[`${environmentName}`] as any
-  jsonSettings.env.region = regionEnvironment
 
-  const settings = jsonSettings as ConfigBuild
-  settings.ssmParameters = Parameters
-  settings.envVariables = {}
-  settings.projectName = Config.projectName as string
-  settings.organizationId = Config.organizationId as string
-  // console.log(settings)
+  const settings: ConfigBuild = {
+    envVariables: {},
+    projectName: Config.projectName as string,
+    ssmParameters: Parameters,
+    envName: environmentName,
+    env: {
+      account: process.env.AWS_ACCOUNT || jsonSettings.env.account,
+      region: process.env.AWS_DEFAULT_REGION || jsonSettings.env.region,
+    },
+  }
+
   return settings
 }
--- config/config.ts
+++ config/config.ts
@@ -4,25 +4,8 @@ export const Config: { [key: string]: unknown } = {
   dev: {
     env: {
       account: '924245329070',
+      region: 'us-east-1',
     },
-    network: [
-      {
-        region: 'us-east-1',
-        vpcId: 'vpc-0dcbcef5f4e12117c',
-        vpcCIDR: '10.0.0.0/16',
-        securityGroup: 'sg-0f8378f007a7ebaac',
-        subnets: [
-          {
-            subnetId: 'subnet-006cf7375564cda72',
-            az: 'us-east-1b',
-          },
-          {
-            subnetId: 'subnet-0354a9bf358503153',
-            az: 'us-east-1a',
-          },
-        ],
-      },
-    ],
     envName: 'dev',
   },
 }
