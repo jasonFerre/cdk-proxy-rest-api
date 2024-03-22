b0ca30501dec26052127254946b3ab88756bd63a
JasonFerre <geysonb04@gmail.com>
2024-03-22 16:04:21 +0000

    tests

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@jest/types'
 import * as path from 'path'
 
-const rootDir = path.resolve('.')
+const rootDir = path.resolve(__dirname, '.')
 const baseTestDir = `${rootDir}/tests/api/services`
 const fileDir = '<rootDir>/api/services/*.ts'
 
